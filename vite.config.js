3a42c4e585587a82c587c639804e13fc384dfc8a
tiago <tgszwdv@gmail.com>
2024-12-10 14:08:06 -0400

    test

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}',
+    './src/**/*.{html,css}', // Adicionando esta linha pode ajudar a garantir a inclusão do CSS
+  ],
   plugins: [react()],
-})
+});
